8572d5578ee76bb7b5319d0d82505f36c145a205
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2023-05-11 22:40:03 +0200

    added a script to plot success rates vs episodes

---
+++ value_based_drl/plot_success_rates.py
@@ -0,0 +1,36 @@
+import os
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_success_rates(ARGS):
+    arr_success_rates = np.load(ARGS.file_npy)
+
+    fig = plt.figure()
+    plt.plot(np.arange(1, len(arr_success_rates) + 1), arr_success_rates)
+    plt.title("test success rates", fontsize=20)
+    plt.xlabel("episode", fontsize=20)
+    plt.ylabel("success rate (%)", fontsize=20)
+    plt.grid()
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.show()
+    return
+
+def main():
+    file_npy = "dqn_simple/test_success_rates.npy"
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument("--file_npy", default=file_npy,
+        type=str, help="full path to numpy file with test success rates")
+
+    ARGS, unparsed = parser.parse_known_args()
+    plot_success_rates(ARGS)
+    return
+
+if __name__ == "__main__":
+    main()
