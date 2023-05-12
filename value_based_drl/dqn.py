a6ed0664600a32f2e81b0eeaa9df0b796ac3e5ed
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2023-05-12 14:09:00 +0200

    fixes to residual network based architecture

--- value_based_drl/dqn.py
+++ value_based_drl/dqn.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 class DQNSimple(nn.Module):
     def __init__(self, num_actions):
-        super(DQNSimple, self).__init__()
+        super().__init__()
         self.num_actions = num_actions
         self.list_feats = [32, 64, 128]
 
@@ -33,7 +33,7 @@ class DQNSimple(nn.Module):
 
 class DQNSimpleNew(nn.Module):
     def __init__(self, num_actions):
-        super(DQNSimpleNew, self).__init__()
+        super().__init__()
         self.num_actions = num_actions
         self.list_feats = [32, 64]
 
@@ -60,18 +60,24 @@ class DQNSimpleNew(nn.Module):
 
 class DQNResidual(nn.Module):
     def __init__(self, num_actions):
-        super(DQNResidual, self).__init__()
+        super().__init__()
         self.num_actions = num_actions
         self.list_feats = [32, 64, 128]
 
-        self.init_conv_layer = nn.Sequential(
-            nn.Conv2d(4, self.list_feats[0], kernel_size=7, stride=2, padding=1),
-            nn.ReLU(inplace=True),
+        self.init_conv = nn.Sequential(
+            nn.Conv2d(4, self.list_feats[0], kernel_size=7, stride=2, padding=3, bias=False),
         )
 
-        self.residual_conv_block_1 = nn.Conv2d(self.list_feats[0], self.list_feats[1], kernel_size=3, stride=1, padding=2, bias=False)
-        self.residual_conv_block_2 = nn.Conv2d(self.list_feats[1], self.list_feats[2], kernel_size=3, stride=1, padding=2, bias=False)
-        self.relu = nn.ReLU(inplace=True)
+        self.conv_1 = nn.Sequential(
+            nn.Conv2d(self.list_feats[0], self.list_feats[1], kernel_size=3, stride=2, padding=2, bias=False),
+        )
+
+        self.conv_2 = nn.Sequential(
+            nn.Conv2d(self.list_feats[1], self.list_feats[2], kernel_size=3, stride=2, padding=1, bias=False),
+        )
+
+        self.residual_conv_block_1 = nn.Conv2d(self.list_feats[1], self.list_feats[1], kernel_size=3, stride=1, padding=1, bias=False)
+        self.residual_conv_block_2 = nn.Conv2d(self.list_feats[2], self.list_feats[2], kernel_size=3, stride=1, padding=1, bias=False)
 
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.linear_layer = nn.Linear(128, self.num_actions)
@@ -81,23 +87,26 @@ class DQNResidual(nn.Module):
 
         if num_features == self.list_feats[1]:
             output_features = self.residual_conv_block_1(input_features)
-            output_features = self.relu(output_features)
+            output_features = F.relu(output_features)
             output_features = self.residual_conv_block_1(output_features)
         else:
             output_features = self.residual_conv_block_2(input_features)
-            output_features = self.relu(output_features)
+            output_features = F.relu(output_features)
             output_features = self.residual_conv_block_2(input_features)
 
-        residual_features += output_features
-        residual_features = self.relu(residual_features)
+        output_features += residual_features
+        output_features = F.relu(output_features)
 
-        return residual_features
+        return output_features
 
     def forward(self, state):
-        init_conv_features = self.init_conv_layer(state)
+        init_conv_features = F.relu(self.init_conv(state))
 
-        residual_features_1 = self.residual_block(init_conv_features, num_features=self.list_feats[1])
-        residual_features_2 = self.residual_block(residual_features, num_features=self.list_feats[2])
+        conv_features_1 = F.relu(self.conv_1(init_conv_features))
+        residual_features_1 = self.residual_block(conv_features_1, num_features=self.list_feats[1])
+
+        conv_features_2 = F.relu(self.conv_2(residual_features_1))
+        residual_features_2 = self.residual_block(conv_features_2, num_features=self.list_feats[2])
 
         dense_features = self.avg_pool(residual_features_2)
         dense_features = torch.flatten(dense_features, 1)
